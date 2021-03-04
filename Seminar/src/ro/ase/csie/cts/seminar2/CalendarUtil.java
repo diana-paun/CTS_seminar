986465d1c453a1e1efb13c8a91fae97fa87cc4f1
DianaPaun <paundiana16@stud.ase.ro>
2021-03-04 14:45:25 +0200

    exceptie costum

--- Seminar/src/ro/ase/csie/cts/seminar2/CalendarUtil.java
+++ Seminar/src/ro/ase/csie/cts/seminar2/CalendarUtil.java
@@ -1,8 +1,11 @@
 package ro.ase.csie.cts.seminar2;
 
+import java.text.DateFormatSymbols;
+import java.util.Locale;
+
 public class CalendarUtil {
 
-	public String weekDay(int day) {
+	public String weekDay(int day) throws IcorectDayException {
 		if (day == 1)
 			return "sunday";
 		else if (day == 2)
@@ -18,11 +21,11 @@ public class CalendarUtil {
 		else if (day == 7)
 			return "Saturday";
 
-		return null;
+		throw new IcorectDayException("Only 7 days in a week");
 	}
 
 	// inlocuim if-else cu switch
-	public String weekDay2(int day) {
+	public String weekDay2(int day)  throws IcorectDayException {
 		switch (day) {
 		case 1:
 			return "sunday";
@@ -39,13 +42,23 @@ public class CalendarUtil {
 		case 7:
 			return "Saturday";
 		default:
-			return null;
+			throw new IcorectDayException("Only 7 days in a week");
 		}
 	}
 
 	// vector
-	public String weekDay3(int day) {
+	public String weekDay3(int day) throws IcorectDayException {
+		if(day < 1 || day > 7)
+			throw new IcorectDayException("Only 7 days in a week");
 		String[] weekDays= {"Sunday", "Monday","Tuesday","Wednesday","Friday","Saturday"};
 		return weekDays[day-1];
+	}
+	
+	//new DateFormat
+	public String weekDay4(int day) throws IcorectDayException {
+		if(day < 1 || day > 7)
+			throw new IcorectDayException("Only 7 days in a week");
+		String [] days = new DateFormatSymbols().getInstance(Locale.ITALIAN).getWeekdays();
+		return days[day-1];
 	}
 }
--- Seminar/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar/src/ro/ase/csie/cts/seminar2/Main.java
@@ -3,13 +3,20 @@ package ro.ase.csie.cts.seminar2;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		CalendarUtil cu = new CalendarUtil();
+		try {
+			System.out.println(cu.weekDay(3).toUpperCase());
+			
+			System.out.println(cu.weekDay2(3));//cu switch
+			
+			System.out.println(cu.weekDay3(3));//cu vector
+			
+			System.out.println(cu.weekDay4(3)); //new DateFormat
 		
-		System.out.println(cu.weekDay(3));
-		
-		System.out.println(cu.weekDay2(3));//cu switch
-		
-		System.out.println(cu.weekDay3(3));//cu vector
-	}
+		} catch (IcorectDayException e) {
+			System.out.println(e.getMessage());
+		}
 
+	}
 }

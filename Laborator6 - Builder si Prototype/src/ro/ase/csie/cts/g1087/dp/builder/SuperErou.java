013d584eb23022d7997b5944d5676e5502bfdd18
Diana Păun <d8paun@gmail.com>
2021-03-31 15:30:24 +0300

    Prototype - sf seminar

--- Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/SuperErou.java
+++ Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/SuperErou.java
@@ -3,23 +3,23 @@ package ro.ase.csie.cts.g1087.dp.builder;
 public class SuperErou {
 
 	//definim atributele - contextul builder
-	String nume;
-	int puncteViata;
+	private String nume;
+	private int puncteViata;
 	
-	boolean esteNegativ;
-	boolean esteRanitGrav;
+	private boolean esteNegativ;
+	private boolean esteRanitGrav;
 	
-	InterfataArma armaStanga;
-	InterfataArma armaDreapta;
-	InterfataSuperPutere  superPutere;
-	InterfataSuperPutere superSuperPutere;
+	private InterfataArma armaStanga;
+	private InterfataArma armaDreapta;
+	private InterfataSuperPutere  superPutere;
+	private InterfataSuperPutere superSuperPutere;
 	
 	//constructor default
-	public SuperErou() {
+	private SuperErou() {
 		
 	}
 
-	public SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
+	private SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
 			InterfataArma armaDreapta, InterfataSuperPutere superPutere, InterfataSuperPutere superSuperPutere) {
 		super();
 		this.nume = nume;
@@ -30,6 +30,51 @@ public class SuperErou {
 		this.armaDreapta = armaDreapta;
 		this.superPutere = superPutere;
 		this.superSuperPutere = superSuperPutere;
+	}
+	
+	public static class SuperErouBuilder {
+		SuperErou superErou; //referinta atribute
+		
+		public SuperErouBuilder(String nume, int puncteViata) {
+			this.superErou = new SuperErou();
+			this.superErou.nume = nume;
+			this.superErou.puncteViata=puncteViata;
+			
+		}
+		
+		public SuperErouBuilder esteNegativ() {
+			this.superErou.esteNegativ=true;
+			return this; //this = SuperErouBuilder - IMPORTANT!!!
+		}
+		
+		public SuperErouBuilder esteRanit() {
+			this.superErou.esteRanitGrav=true;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaStanga(InterfataArma arma) {
+			this.superErou.armaStanga = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaDreapta(InterfataArma arma) {
+			this.superErou.armaDreapta = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperPutere(InterfataSuperPutere superPutere) {
+			this.superErou.superPutere = superPutere;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperSuperPutere(InterfataSuperPutere superSuperPutere) {
+			this.superErou.superSuperPutere = superSuperPutere;
+			return this;
+		}
+		
+		public SuperErou build() {
+			return this.superErou;
+		}
 	}
 	
 	
--- Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/TestBuilder.java
+++ Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/TestBuilder.java
@@ -4,15 +4,36 @@ public class TestBuilder {
 
 	public static void main(String[] args) {
 
-		//1 construieste obj
-		SuperErou superErou = new SuperErou();
-		//2 initializarea atributelor
-		superErou.nume="Superman";
 		
+//		//1 construieste obj
+//		SuperErou superErou = new SuperErou();
+//		//2 initializarea atributelor
+//		superErou.nume="Superman";
+//		
+//		
+//		SuperErou superErou2 = 
+//				new SuperErou("Batman", 100, false, false, new Arma(), null, null, null); 
 		
-		SuperErou superErou2 = 
-				new SuperErou("Batman", 100, false, false, new Arma(), null, null, null);
+		SuperErou superman = 
+				new SuperErou.SuperErouBuilder("Superman", 100)
+				.setArmaDreapta(new Arma())
+				.setArmaStanga(new Arma())
+				.build();
 		
+		SuperErou joker = 
+				new SuperErou.SuperErouBuilder("Joker", 200)
+				.esteNegativ()
+				.esteRanit()
+				.setArmaDreapta(new Arma())
+				.build();
+		
+		//Director
+		SuperErou joker2 = 
+				new SuperErou.SuperErouBuilder("Joker", 200)
+				.esteNegativ()
+				.esteRanit()
+				.setArmaDreapta(new Arma())
+				.build();
 	}
 
 }

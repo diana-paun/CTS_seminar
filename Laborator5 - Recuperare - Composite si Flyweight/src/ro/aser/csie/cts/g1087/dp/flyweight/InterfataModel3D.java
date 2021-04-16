1f186b23d3d03aa14a42d3d489d74d41ab1e9f24
diana-paun <79836786+diana-paun@users.noreply.github.com>
2021-04-16 11:26:49 +0300

    Implementare Flyweight

---
+++ Laborator5 - Recuperare - Composite si Flyweight/src/ro/aser/csie/cts/g1087/dp/flyweight/InterfataModel3D.java
@@ -0,0 +1,8 @@
+package ro.aser.csie.cts.g1087.dp.flyweight;
+
+public interface InterfataModel3D {
+
+	public void incarcaModel3D(String fisier);
+	
+	public void afisareEcran(ContextEcran context);
+}

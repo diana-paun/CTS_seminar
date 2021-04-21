2184e321920f6b8ac7bd4b3d9cf1b5ce8bb151eb
Diana Păun <d8paun@gmail.com>
2021-04-21 14:41:50 +0300

    Implementare Proxy

---
+++ Laborator9 - Proxy Strategy State/src/ro/ase/csie/cts/g1087/dp/proxy/ModulLogin.java
@@ -0,0 +1,29 @@
+package ro.ase.csie.cts.g1087.dp.proxy;
+
+public class ModulLogin implements InterfataModulLogin{
+
+	String ipServer;
+	int port;
+	
+	public ModulLogin(String ipServer, int port) {
+		super();
+		this.ipServer = ipServer;
+		this.port = port;
+	}
+
+	@Override
+	public boolean login(String username, String password) {
+		if(username.equals("admin") && password.equals("admin12")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean verificaStatusServer() {
+		return true;
+	}
+
+}

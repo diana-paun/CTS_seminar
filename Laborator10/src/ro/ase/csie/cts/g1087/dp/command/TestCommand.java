a8aafd5273aef16ee016c6d4b6a1d4dd3f289599
Diana Păun <d8paun@gmail.com>
2021-05-12 14:25:15 +0300

    Adaugare manager comenzi pt Command

--- Laborator10/src/ro/ase/csie/cts/g1087/dp/command/TestCommand.java
+++ Laborator10/src/ro/ase/csie/cts/g1087/dp/command/TestCommand.java
@@ -4,12 +4,24 @@ public class TestCommand {
 
 	public static void main(String[] args) {
 		
+		ManagerTaskuri manager = new ManagerTaskuri();
+		
 		TaskAsincron autoSave = new TaskToDo(new ModulAutosave(), "Autosave", 5);
 		TaskAsincron autoUpdate = new TaskToDo(new ModulUpdateClient(), "Update v2", 20);
 
+		manager.adaugaTask(autoSave);
+		manager.adaugaTask(autoUpdate);
+		
+		System.out.println("Jocul e activ");
+		
+		manager.executaTask();
+		manager.executaTask();
+		
+		System.out.println("Jocul e activ");
+		
 		//executie task-uri asincrone (manager)
-		autoSave.executaTask();
-		autoUpdate.executaTask();
+		//autoSave.executaTask();
+		//autoUpdate.executaTask();
 		
 	}
 }
